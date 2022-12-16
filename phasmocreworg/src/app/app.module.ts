865256ef43f38f916304a497cbeb8814f3f18fe5
ArturFoden <fodena@tcd.ie>
2022-12-16 17:59:52 +0000

    updated routing to work with bookmarks

--- phasmocreworg/src/app/app.module.ts
+++ phasmocreworg/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhasmoFooterComponent } from './phasmo-footer/phasmo-footer.component';
 import { PhasmoHeaderComponent } from './phasmo-header/phasmo-header.component';
 import { PhasmoMainContentComponent } from './phasmo-main-content/phasmo-main-content.component';
 import { PhasmoAboutComponent } from './phasmo-about/phasmo-about.component';
 import { PhasmoNavbarComponent } from './phasmo-navbar/phasmo-navbar.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { PhasmoHomeComponent } from './phasmo-home/phasmo-home.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -26,10 +25,12 @@ import { PhasmoHomeComponent } from './phasmo-home/phasmo-home.component';
     RouterModule.forRoot([
       { path: 'home', component: PhasmoHomeComponent },
       { path: 'about-us', component: PhasmoAboutComponent },
+      { path: '', redirectTo: '', pathMatch: 'full' },
+      { path: '**', component: AppComponent },
       // { enableTracing: true } // <-- debugging purposes only
     ]),
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
